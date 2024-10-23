dfa82347dcfdc3d64a24ba82d82841a23803c32d
xaman27x <136126298+xaman27x@users.noreply.github.com>
2024-10-23 23:30:27 +0530

    Candidate Test Preview Added

--- lib/candidate_components/candidate_home_page.dart
+++ lib/candidate_components/candidate_home_page.dart
@@ -1,4 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intelliquiz/models/auth.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+String instructionBuilder({
+  required int questionCount,
+  required int durationInMin,
+}) {
+  return '''
+TEST INSTRUCTIONS:\n
+- Duration: The test will last for $durationInMin Minutes.\n
+- Questions: This test contains $questionCount questions.\n
+- Do not switch tabs or leave the test window, or you may be disqualified.\n
+- Ensure a stable internet connection.\n
+- Read each question carefully.\n
+- Submit before the time expires.\n
+- For technical issues, contact sdsadmin@gmail.com.\n
+- Cheating or plagiarism is strictly prohibited.\n
+- All the best!
+''';
+}
 
 class CandidateHomePage extends StatefulWidget {
   const CandidateHomePage({super.key});
@@ -8,8 +29,186 @@ class CandidateHomePage extends StatefulWidget {
 }
 
 class _CandidateHomePage extends State<CandidateHomePage> {
+  late Stream<QuerySnapshot> finalTestStream;
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+
+  @override
+  void initState() {
+    finalTestStream =
+        FirebaseFirestore.instance.collection('Tests').snapshots();
+    super.initState();
+    return;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    return;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        title: Image.asset(
+          'images/sds.png',
+          scale: 11,
+          fit: BoxFit.fitHeight,
+        ),
+        centerTitle: true,
+        actions: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+            onPressed: () {
+              Navigator.pop(context);
+              signOut();
+            },
+            child: Text(
+              "SIGN OUT",
+              style: GoogleFonts.raleway(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          )
+        ],
+      ),
+      body: Container(
+        color: Colors.grey[350],
+        child: StreamBuilder<QuerySnapshot>(
+          stream: finalTestStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  "An Error Occured",
+                  style: GoogleFonts.raleway(
+                    color: Colors.black,
+                  ),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var data = snapshot.data!.docs[index];
+                  int? questionCount = data['questions'].length;
+                  int? durationInMin = data['durationInMin'];
+                  DateTime scheduledTime =
+                      (data['scheduledTime'] as Timestamp).toDate();
+                  return Card(
+                    child: ListTile(
+                      leading: IconButton(
+                        icon: const Icon(
+                          Icons.school_sharp,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          const AlertDialog();
+                        },
+                      ),
+                      title: Text(
+                        "Test: ${data['testName']}",
+                        style: GoogleFonts.raleway(
+                          color: Colors.black,
+                          fontSize: 19,
+                        ),
+                      ),
+                      subtitle: Row(
+                        children: [
+                          Text(
+                            "Questions: $questionCount",
+                            style: GoogleFonts.raleway(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Scheduled Time: $scheduledTime",
+                            style: GoogleFonts.raleway(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                        ),
+                        onPressed: () => {
+                          if (DateTime.now().isBefore(scheduledTime))
+                            {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text(
+                                      "Test Has Not Yet Started!",
+                                      style: GoogleFonts.raleway(),
+                                    ),
+                                  );
+                                },
+                              )
+                            }
+                          else if ((DateTime.now()
+                                  .isAtSameMomentAs(scheduledTime)) ||
+                              (DateTime.now().isAfter(scheduledTime) &&
+                                  DateTime.now().isBefore(
+                                    scheduledTime.add(
+                                      const Duration(
+                                        minutes: 15,
+                                      ),
+                                    ),
+                                  )))
+                            {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    content: Text(
+                                      instructionBuilder(
+                                        questionCount: questionCount!,
+                                        durationInMin: durationInMin!,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              )
+                            }
+                        },
+                        child: Text(
+                          'START',
+                          style: GoogleFonts.raleway(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intelliquiz/admin_components/admin_home_page.dart';
+import 'package:intelliquiz/candidate_components/candidate_home_page.dart';
 import 'package:intelliquiz/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:intelliquiz/root_components/root_home_page.dart';
-import 'package:intelliquiz/shared_components/intro_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,12 +14,12 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'IntelliQuiz',
-      home: RootHomePage(),
+      home: CandidateHomePage(),
     );
   }
 }
